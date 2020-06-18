2231ce95655ffdd6010d7c2b067c276f6abcbe84
Steve Jones <steve.jones763@gmail.com>
2020-06-18 19:05:19 +0100

    Add default Xcode files

---
+++ AnimationDemo/AnimationDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimationDemo
+//
+//  Created by Steve Jones on 18/06/2020.
+//  Copyright © 2020 95Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
